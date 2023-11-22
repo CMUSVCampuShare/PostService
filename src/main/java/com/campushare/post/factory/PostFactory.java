c13dff57d31658e507d68aae80d70843f8b34ea2
jraviS2023 <jravi@andrew.cmu.edu>
2023-11-21 19:13:04 -0800

    adding new status

--- src/main/java/com/campushare/post/factory/PostFactory.java
+++ src/main/java/com/campushare/post/factory/PostFactory.java
@@ -1,14 +1,9 @@
 package com.campushare.post.factory;
 
-import com.campushare.post.model.Comment;
 import com.campushare.post.model.Post;
 import com.campushare.post.request.PostRequest;
-import com.campushare.post.utils.Status;
 import com.campushare.post.utils.Type;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.List;
 
 @Component
 public abstract class PostFactory {
